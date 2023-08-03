3dd9216302ba5c09c04883cc1a9d8deff77d5ff9
Olga Nadongo <winnienadongo@gmail.com>
2023-08-03 10:01:46 +0300

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+        <h1 className=''>Geoffrey Anyona</h1>
+        </div>
+  )
+}
+
+export default Navbar
